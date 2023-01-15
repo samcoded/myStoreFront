4cfe77fcd912c8c5ccca4800318cb842e3ebb99d
Samuel Anozie <samuelanozie10@yahoo.com>
2023-01-15 17:29:19 +0100

    feat: basic product list component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  products: Product[] = [];
 
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.productService.index().subscribe((products) => {
+      this.products = products;
+    });
+  }
+
+  addToCart(productId: number, quantity: number): void {
+    this.cartService.addToCart(productId, quantity);
+  }
 }
