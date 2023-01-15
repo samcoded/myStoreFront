fb25b88d932eaa96a0f77f9f31c5c6abb1cb56b7
Samuel Anozie <samuelanozie10@yahoo.com>
2023-01-15 17:27:11 +0100

    feat: cart components basic

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  cartItems: any[] = [];
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getProducts();
+  }
+
+  removeItem(productId: number): void {
+    this.cartService.removeItem(productId);
+    this.cartItems = this.cartService.getProducts();
+  }
+
+  removeOneItem(productId: number): void {
+    this.cartService.removeOneItem(productId);
+    this.cartItems = this.cartService.getProducts();
+  }
 }
