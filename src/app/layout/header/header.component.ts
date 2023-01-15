af20b9ab62f362e1922d3b812c3aa0c5ad893dd8
Samuel Anozie <samuelanozie10@yahoo.com>
2023-01-15 17:27:31 +0100

    feat: basic header components

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private cartService: CartService) {}
 
+  totalCartItems: number = this.cartService.getTotalItems();
+
+  ngOnInit(): void {}
+  ngDoCheck(): void {
+    this.totalCartItems = this.cartService.getTotalItems();
+  }
 }
